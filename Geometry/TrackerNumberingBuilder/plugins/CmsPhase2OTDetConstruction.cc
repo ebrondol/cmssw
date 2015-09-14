35561d7ead1ad6a21b109c50ae8a7b66a31ad168
erica brondolin <erica.brondolin@cern.ch>
2015-09-14 16:09:46 +0200

    all barrel modules became Stacks

--- Geometry/TrackerNumberingBuilder/plugins/CmsPhase2OTDetConstruction.cc
+++ Geometry/TrackerNumberingBuilder/plugins/CmsPhase2OTDetConstruction.cc
@@ -9,17 +9,18 @@ void CmsPhase2OTDetConstruction::buildComponent(
 					GeometricDet *mother, 
 					std::string attribute){
   
+  LogTrace("DetConstruction") << " CmsPhase2OTDetConstruction::buildComponent ";
   GeometricDet * det  = new GeometricDet(&fv,theCmsTrackerStringToEnum.type(ExtractStringFromDDD::getString(attribute,&fv)));
   if (theCmsTrackerStringToEnum.type(ExtractStringFromDDD::getString(attribute,&fv)) ==  GeometricDet::OTPhase2Stack){
 
     bool dodets = fv.firstChild(); // descend to the first Layer
     while (dodets) {
+      LogTrace("DetConstruction") << " new child! ";
       buildSmallDets(fv,det,attribute);
       dodets = fv.nextSibling(); // go to next layer
     }
     fv.parent();
   }
-  
   mother->addComponent(det);
 }
 
@@ -32,15 +33,16 @@ void CmsPhase2OTDetConstruction::buildSmallDets(
   static const std::string isInner = "TrackerInnerDetectors";
   static const std::string isOuter = "TrackerOuterDetectors";
   if (ExtractStringFromDDD::getString(isInner,&fv) == "true"){
+    LogTrace("DetConstruction") << " inner ";
     uint32_t temp = 1;
     det->setGeographicalID(DetId(temp));
   } else if (ExtractStringFromDDD::getString(isOuter,&fv) == "true"){
+    LogTrace("DetConstruction") << " outer ";
     uint32_t temp = 2;
     det->setGeographicalID(DetId(temp));
   } else {
     edm::LogError("DetConstruction") << " module defined as a Stack but not inner either outer!? ";
   }
-  
   mother->addComponent(det); 
 }
 
