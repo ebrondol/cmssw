0cfbe13d11f556c388703159274005ae98105931
erica brondolin <erica.brondolin@cern.ch>
2016-06-03 17:51:41 +0200

    insert SiPhase2RecHitMatcher

---
+++ RecoLocalTracker/SiPhase2VectorHitBuilder/plugins/SiPhase2RecHitMatcherESProducer.cc
@@ -0,0 +1,35 @@
+#include "RecoLocalTracker/SiPhase2VectorHitBuilder/plugins/SiPhase2RecHitMatcherESProducer.h"
+
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/ModuleFactory.h"
+#include "FWCore/Framework/interface/ESProducer.h"
+
+SiPhase2RecHitMatcherESProducer::SiPhase2RecHitMatcherESProducer(const edm::ParameterSet & p) 
+{
+  std::cout << "ci siamo! :SiPhase2RecHitMatcherESProduce" << std::endl;
+  name = p.getParameter<std::string>("ComponentName");
+  pset_ = p;
+  setWhatProduced(this,name);
+}
+
+std::shared_ptr<VectorHitBuilderEDProducer> SiPhase2RecHitMatcherESProducer::
+produce(const TkPhase2OTCPERecord & iRecord)
+{ 
+  if( name == "SiPhase2VectorHitMatcher" ){
+    matcher_  = std::make_shared<VectorHitBuilderEDProducer>(pset_);
+//    matcher_  = std::shared_ptr<VectorHitBuilderEDProducer>(new VectorHitBuilderEDProducer(pset_));
+
+    edm::ESHandle<TrackerGeometry> tGeomHandle;
+    edm::ESHandle<TrackerTopology> tTopoHandle;
+  
+    iRecord.getRecord<TrackerDigiGeometryRecord>().get(tGeomHandle);
+    iRecord.getRecord<IdealMagneticFieldRecord>().get(tTopoHandle);
+
+    matcher_->algo()->initTkGeom(tGeomHandle);
+    matcher_->algo()->initTkTopo(tTopoHandle);
+  }
+  return matcher_;
+}
+
+
---
+++ RecoLocalTracker/SiPhase2VectorHitBuilder/python/SiPhase2RecHitMatcher_cfi.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+SiPhase2RecHitMatcherESProducer = cms.ESProducer("SiPhase2RecHitMatcherESProducer",
+    ComponentName = cms.string('SiPhase2VectorHitMatcher'),
+     Clusters = cms.InputTag('siPhase2Clusters'),
+     offlinestubs = cms.string('vectorHits'),
+     maxVectorHits = cms.int32(999999999),
+     maxVectorHitsinaStack = cms.int32(999),
+     Algorithm = cms.string('VectorHitBuilderAlgorithm'),
+     CPE = cms.ESInputTag('StripCPEfromTrackAngleESProducer:StripCPEfromTrackAngle'),
+#    NSigmaInside = cms.double(3.0)
+)
+
+
