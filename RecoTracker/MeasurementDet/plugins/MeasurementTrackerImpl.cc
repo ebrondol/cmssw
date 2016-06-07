5682a9546683225fceae20d83b645ec805481da2
erica brondolin <erica.brondolin@cern.ch>
2016-06-07 14:47:49 +0200

    passing the matcher to the meas trk, but still not using it

--- RecoTracker/MeasurementDet/plugins/MeasurementTrackerImpl.cc
+++ RecoTracker/MeasurementDet/plugins/MeasurementTrackerImpl.cc
@@ -81,6 +81,7 @@ MeasurementTrackerImpl::MeasurementTrackerImpl(const edm::ParameterSet&         
 				       const PixelClusterParameterEstimator* pixelCPE,
 				       const StripClusterParameterEstimator* stripCPE,
 				       const SiStripRecHitMatcher*  hitMatcher,
+				       const VectorHitBuilderEDProducer*  ph2hitMatcher,
 				       const TrackerGeometry*  trackerGeom,
 				       const GeometricSearchTracker* geometricSearchTracker,
                                        const SiStripQuality *stripQuality,
@@ -96,7 +97,7 @@ MeasurementTrackerImpl::MeasurementTrackerImpl(const edm::ParameterSet&         
   name_(conf.getParameter<std::string>("ComponentName")),
   theStDetConditions(hitMatcher,stripCPE),
   thePxDetConditions(pixelCPE),
-  thePhase2DetConditions(phase2OTCPE)
+  thePhase2DetConditions(ph2hitMatcher, phase2OTCPE)
 {
   this->initialize();
   this->initializeStripStatus(stripQuality, stripQualityFlags, stripQualityDebugFlags);
