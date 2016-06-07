0dfa36cea606725d78249618e7d6b6b302fdf44d
erica brondolin <erica.brondolin@cern.ch>
2016-06-07 15:17:49 +0200

    adding Vh and matcher in tkstackmeasdet
    add vh in TkCloner
    
    isPhase2() in vh
    
    fix TkStackMeasurementDet::measurements
    
    set GeomDet for vh
    
    fix bugs all around
    
    fix vh validation

--- RecoTracker/MeasurementDet/plugins/MeasurementTrackerEventProducer.cc
+++ RecoTracker/MeasurementDet/plugins/MeasurementTrackerEventProducer.cc
@@ -234,6 +234,7 @@ MeasurementTrackerEventProducer::updateStrips( const edm::Event& event, StMeasur
 void 
 MeasurementTrackerEventProducer::updatePhase2OT( const edm::Event& event, Phase2OTMeasurementDetSet & thePh2OTDets ) const {
 
+  thePh2OTDets.setEmpty();
 
   // Phase2OT Clusters
   if ( isPhase2 ) {
