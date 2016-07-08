b289fcde48b4eed2635b1245b528bb8ecbc7a5de
erica brondolin <erica.brondolin@cern.ch>
2016-07-08 17:09:45 +0200

    building tsos
    returning global pos&err of clusters in the VH
    
    fixing const method and printouts

--- RecoTracker/MeasurementDet/src/SeedingOTEDProducer.cc
+++ RecoTracker/MeasurementDet/src/SeedingOTEDProducer.cc
@@ -8,13 +8,12 @@
 #include "RecoTracker/MeasurementDet/interface/MeasurementTrackerEvent.h"
 #include "TrackingTools/KalmanUpdators/interface/Chi2MeasurementEstimator.h"
 
+#include "DataFormats/TrajectoryState/interface/LocalTrajectoryParameters.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 SeedingOTEDProducer::SeedingOTEDProducer(edm::ParameterSet const& conf):
   tkMeasEvent( consumes<MeasurementTrackerEvent>(conf.getParameter<edm::InputTag>("trackerEvent")) )
 {
-  std::cout << "SeedingOT::init" << std::endl;
-  cpe = conf.getParameter<std::string>("phase2OTCPE");
   vhProducer = consumes< VectorHitCollectionNew >(edm::InputTag(conf.getParameter<edm::InputTag>("src")));
   produces< VectorHitCollectionNew >();
 }
@@ -26,7 +25,6 @@ void SeedingOTEDProducer::fillDescriptions(edm::ConfigurationDescriptions& descr
   edm::ParameterSetDescription desc;
   desc.add<edm::InputTag>("src", edm::InputTag("siPhase2VectorHits","vectorHitsAccepted"));
   desc.add<edm::InputTag>("trackerEvent", edm::InputTag("MeasurementTrackerEvent"));
-  desc.add<std::string>("phase2OTCPE", std::string("Phase2StripCPEGeometric"));
   descriptions.add("SeedingOTEDProducer", desc);
 }
     
@@ -61,39 +59,48 @@ void SeedingOTEDProducer::produce(edm::Event& event, const edm::EventSetup& es)
   // Get the vector hits
   edm::Handle< VectorHitCollectionNew > vhs;
   event.getByToken(vhProducer, vhs);
-
-  edm::ESHandle< Phase2StripCPEGeometric > parameterestimator;
+/*
+  edm::ESHandle< ClusterParameterEstimator<Phase2TrackerCluster1D> > parameterestimator;
   es.get<TkStripCPERecord>().get(cpe, parameterestimator); 
   const Phase2StripCPEGeometric & cpeOT(*parameterestimator);
-
-  run(vhs, cpeOT, barrelOTLayer2);
+*/
+  run(vhs, barrelOTLayer2);
 
   std::cout << "SeedingOT::produce() end" << std::endl;
 
 }
 
 void SeedingOTEDProducer::run(edm::Handle< VectorHitCollectionNew > VHs, 
-                              const Phase2StripCPEGeometric & cpe,
                               const DetLayer* layerSearch){
 
-  const VectorHitCollectionNew& input = *VHs;
+  std::vector<VectorHit> VHseeds = collectVHsInput(VHs);
+  std::cout << "VH seeds = " << VHseeds.size() << std::endl;
 
-  //seeds are built in the L3 of the OT
-  std::vector<VectorHit> VHseeds;
-  if (input.size() > 0 ) {  
-    std::cout << "initial VH collection size = " << input.size() << std::endl;
-    for (auto DSViter : input) {
-      std::cout << "the ID from the DSViter = " << DSViter.id() << std::endl; 
-      std::cout << "the Layer is " << checkLayer(DSViter.id()) << std::endl; 
-      if(checkLayer(DSViter.id()) == 3){
-        for(auto vh : DSViter)
-          VHseeds.push_back(vh);
-      }
-    }
-  }
+  for(auto seed : VHseeds){
 
+    std::cout << "\tbuilding a seed for the VH: " << seed << std::endl;
+    int charge = 1;
+    LocalTrajectoryParameters ltpar(seed.localPosition(), seed.localDirection(), charge);
+    float pT = seed.transverseMomentum(magField);
+    std::cout << "\tpT of the vh : " << pT << std::endl;
+    float p = seed.momentum(magField);
+    std::cout << "\tp of the vh : " << p << std::endl;
+    float x = seed.localPosition().x();
+    float y = seed.localPosition().y();
+    float dx = seed.localDirection().x();
+    float dy = seed.localDirection().y(); 
+    LocalTrajectoryParameters ltpar2(1./p, dx, dy, x, y, charge);
+    AlgebraicSymMatrix mat = assign44To55(seed.parametersError());
+    //FIXME::set the error on 1/p
+    //mat[4][4] = seed.curvatureORphi("curvature").second;
+    std::cout << "\tmat : " << mat << std::endl;
+    LocalTrajectoryError lterr(asSMatrix<5>(mat));
+    const TrajectoryStateOnSurface tsos(ltpar, lterr, seed.det()->surface(), magField);
+    std::vector<TrajectoryMeasurement> tmp = layerMeasurements->measurements(*layerSearch, tsos, *propagator, *estimator);
+    std::cout << "\tvh compatibles: " << tmp.size() << std::endl;
+//    for(auto tm : tmp)
+//      auto hitPossible = tob6TM.recHit();
 
-  for(auto seeds : VHseeds){
 
   }
 
@@ -101,13 +108,43 @@ void SeedingOTEDProducer::run(edm::Handle< VectorHitCollectionNew > VHs,
 }
 
 unsigned int SeedingOTEDProducer::checkLayer( unsigned int iidd ){
-
   StripSubdetector strip = StripSubdetector(iidd);
   unsigned int subid = strip.subdetId();
-  if (subid ==  StripSubdetector::TIB || subid ==  StripSubdetector::TOB) { 
+  if (subid ==  StripSubdetector::TIB || subid ==  StripSubdetector::TOB) {
     return tkTopo->layer(iidd);
   }
-
   return 0;
+}
 
+std::vector<VectorHit> SeedingOTEDProducer::collectVHsInput( edm::Handle< VectorHitCollectionNew > VHs ){
+
+  const VectorHitCollectionNew& input = *VHs;
+  //seeds are built in the L3 of the OT
+  std::vector<VectorHit> VHs_layer3;
+  if (input.size() > 0 ) {  
+    std::cout << "initial VH collection size = " << input.size() << std::endl;
+    for (auto DSViter : input) {
+      if(checkLayer(DSViter.id()) == 3){
+        for(auto vh : DSViter)
+          VHs_layer3.push_back(vh);
+      }
+    }
+  }
+
+  std::cout << "VH in layer 3 collection size = " << VHs_layer3.size() << std::endl;
+
+  return VHs_layer3;
+}
+
+AlgebraicSymMatrix SeedingOTEDProducer::assign44To55(AlgebraicSymMatrix mat44){
+  if(mat44.num_row() != 4 || mat44.num_col() != 4)
+    assert("Wrong dimension! This should be a 4x4 matrix!");
+
+  AlgebraicSymMatrix result(5,0);
+  for(int i = 0; i < 4; i++){
+    for(int j = 0; j < 4; j++){
+      result[i][j] = mat44[i][j];
+    }
+  }
+  return result;
 }
