c34ada7c4fc771688a07f850cbc29ad8c45394db
erica brondolin <erica.brondolin@cern.ch>
2017-05-18 16:29:34 +0200

    Starting with compatibility
    Logic for first compatibility test is pretty simple:
    -ordering lower and upper clusters in the precise local coordinate
    -computing a delta
    -checking if lower is compatible with upper within the delta
    VectorHitBuilderAlgorithm has been adapt to save rejected and accepted
    VHits.
    To be fixed:
    - printout are more than needed
    - checkBeforeBuilding function need to be removed
    - when the studies of the rejection are finishes adapting the code to
      make it faster (not doing all the combinatorics)
    - fix the global tracking reconstruction

--- RecoLocalTracker/SiPhase2VectorHitBuilder/interface/VectorHitBuilderAlgorithm.h
+++ RecoLocalTracker/SiPhase2VectorHitBuilder/interface/VectorHitBuilderAlgorithm.h
@@ -32,10 +32,20 @@ class VectorHitBuilderAlgorithm : public VectorHitBuilderAlgorithmBase {
                                                                          const detset & theUpperDetSet);
   bool checkClustersCompatibility(Local3DPoint& posinner, Local3DPoint& posouter, LocalError& errinner, LocalError& errouter);
 
-  std::vector<VectorHit> buildVectorHits(const StackGeomDet * stack,
-                                         edm::Handle< edmNew::DetSetVector<Phase2TrackerCluster1D> > clusters,
-                                         const detset & DSVinner, const detset & DSVouter,
-                                         const std::vector<bool>& phase2OTClustersToSkip = std::vector<bool>());
+  class LocalPositionSort {
+      public: 
+          LocalPositionSort(const TrackerGeometry *geometry, const ClusterParameterEstimator<Phase2TrackerCluster1D>* cpe, const GeomDet * geomDet) : geom_(geometry), cpe_(cpe), geomDet_(geomDet) {}
+          bool operator()(Phase2TrackerCluster1DRef clus1, Phase2TrackerCluster1DRef clus2) const ;
+      private:
+          const TrackerGeometry *geom_;
+          const ClusterParameterEstimator<Phase2TrackerCluster1D>* cpe_;
+          const GeomDet * geomDet_;
+  };
+
+  std::map<VectorHit,bool> buildVectorHits(const StackGeomDet * stack,
+                                           edm::Handle< edmNew::DetSetVector<Phase2TrackerCluster1D> > clusters,
+                                           const detset & DSVinner, const detset & DSVouter,
+                                           const std::vector<bool>& phase2OTClustersToSkip = std::vector<bool>());
 
   VectorHit buildVectorHit(const StackGeomDet* stack, Phase2TrackerCluster1DRef lower, Phase2TrackerCluster1DRef upper);
 
