11cea084456c559bd3cb0d24bdc16b40b057f70b
erica brondolin <erica.brondolin@cern.ch>
2017-06-12 17:36:59 +0200

    different VHit cut for different OT layers

--- RecoLocalTracker/SiPhase2VectorHitBuilder/python/SiPhase2VectorHitBuilder_cfi.py
+++ RecoLocalTracker/SiPhase2VectorHitBuilder/python/SiPhase2VectorHitBuilder_cfi.py
@@ -6,5 +6,7 @@ siPhase2VectorHits = cms.EDProducer("VectorHitBuilderEDProducer",
      maxVectorHits = cms.int32(999999999),
      maxVectorHitsinaStack = cms.int32(999),
      Algorithm = cms.string('VectorHitBuilderAlgorithm'),
+     BarrelCut = cms.vdouble( 0., 0.5, 0.6, 0.8, 0.9, 1.2, 2.), #layers are 6
+     EndcapCut = cms.vdouble( 0., 1., 1., 1., 1., 1.), #disks are 5
      CPE = cms.ESInputTag("phase2StripCPEESProducer", "Phase2StripCPE")
 )
