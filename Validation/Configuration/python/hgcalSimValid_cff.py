849409856613cd75cf56d606890052281cf0c659
Leonardo Cristella <leonardo.cristella@cern.ch>
2020-10-28 11:45:20 +0100

    Full validator for CP with one SimCluter

--- Validation/Configuration/python/hgcalSimValid_cff.py
+++ Validation/Configuration/python/hgcalSimValid_cff.py
@@ -6,7 +6,7 @@ from Validation.HGCalValidation.digiValidation_cff      import *
 from Validation.HGCalValidation.rechitValidation_cff    import *
 from Validation.HGCalValidation.hgcalHitValidation_cfi  import *
 
-from Validation.HGCalValidation.HGCalValidator_cfi import hgcalValidator
+from Validation.HGCalValidation.HGCalValidator_cfi import hgcalValidator, hgcalValidator2
 from Validation.RecoParticleFlow.PFJetValidation_cff import pfJetValidation1 as _hgcalPFJetValidation
 
 from Validation.HGCalValidation.ticlPFValidation_cfi import ticlPFValidation
@@ -15,7 +15,7 @@ hgcalTiclPFValidation = cms.Sequence(ticlPFValidation)
 from Validation.HGCalValidation.ticlTrackstersValidation_cfi import ticlTrackstersValidation
 hgcalTiclTrackstersValidationSequence = cms.Sequence(ticlTrackstersValidation)
 
-hgcalValidatorSequence = cms.Sequence(hgcalValidator)
+hgcalValidatorSequence = cms.Sequence(hgcalValidator + hgcalValidator2)
 hgcalPFJetValidation = _hgcalPFJetValidation.clone(BenchmarkLabel = 'PFJetValidation/HGCAlCompWithGenJet',
     VariablePtBins=[10., 30., 80., 120., 250., 600.],
     DeltaPtOvPtHistoParameter = dict(EROn=True,EREtaMax=3.0, EREtaMin=1.6, slicingOn=True))
