5c6f63eaf8462d8d8c4dbbc26f342ead95ba7552
Erica Brondolin <erica.brondolin@cern.ch>
2020-11-20 15:13:08 +0100

    Clean up and add CaloPart to makeHGCalValidationPlots all

--- Validation/HGCalValidation/plugins/HGCalValidator.cc
+++ Validation/HGCalValidation/plugins/HGCalValidator.cc
@@ -216,7 +216,7 @@ void HGCalValidator::dqmAnalyze(const edm::Event& event,
                                 const Histograms& histograms) const {
   using namespace reco;
 
-  std::cout << "\n===================================================="
+  LogDebug("HGCalValidator") << "\n===================================================="
                              << "\n"
                              << "Analyzing new event"
                              << "\n"
@@ -381,7 +381,7 @@ void HGCalValidator::dqmAnalyze(const edm::Event& event,
                                                     totallayers_to_monitor_);
 
       //General Info on multiclusters
-      std::cout << "\n# of multi clusters with " << label_mcl[wml].process() << ":"
+      LogTrace("HGCalValidator") << "\n# of multi clusters with " << label_mcl[wml].process() << ":"
                                  << label_mcl[wml].label() << ":" << label_mcl[wml].instance() << ": "
                                  << multiClusters.size() << "\n"
                                  << std::endl;
