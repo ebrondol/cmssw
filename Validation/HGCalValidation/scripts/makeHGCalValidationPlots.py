f37bd68d8c1b9a1b8da5fa54af64b1b3ecc4b604
Erica Brondolin <erica.brondolin@cern.ch>
2020-11-23 11:18:06 +0100

    Add 2D plotting for multiple files

--- Validation/HGCalValidation/scripts/makeHGCalValidationPlots.py
+++ Validation/HGCalValidation/scripts/makeHGCalValidationPlots.py
@@ -70,8 +70,6 @@ def main(opts):
                          "kaon-": "-321", "kaon+": "321"}
         hgcaloPart = [hgcalPlots.hgcalCaloParticlesPlotter]
         for i_part, i_partID in particletypes.iteritems() :
-            print(i_part)
-            print(i_partID)
             hgcalPlots.append_hgcalCaloParticlesPlots(sample.files(), i_partID, i_part)
         val.doPlots(hgcaloPart, plotterDrawArgs=drawArgs)
     elif opts.collection==hitValidationLabel:
@@ -91,8 +89,6 @@ def main(opts):
                          "kaon-": "-321", "kaon+": "321"}
         hgcaloPart = [hgcalPlots.hgcalCaloParticlesPlotter]
         for i_part, i_partID in particletypes.iteritems() :
-            print(i_part)
-            print(i_partID)
             hgcalPlots.append_hgcalCaloParticlesPlots(sample.files(), i_partID, i_part)
         val.doPlots(hgcaloPart, plotterDrawArgs=drawArgs)
 
